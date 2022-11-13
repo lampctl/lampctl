f6b36590ff2462bef69863ac4ff7670e272f17d1
Nathan Osman <nathan@quickmediasolutions.com>
2022-11-12 23:13:09 -0800

    Add JSON tags to Bridge DB object.

--- hue/db/bridge.go
+++ hue/db/bridge.go
@@ -2,7 +2,7 @@ package db
 
 // Bridge provides database storage for Hue connection information.
 type Bridge struct {
-	ID       string `gorm:"primaryKey"`
-	Host     string `gorm:"not null"`
-	Username string `gorm:"not null"`
+	ID       string `gorm:"primaryKey" json:"id"`
+	Host     string `gorm:"not null" json:"host"`
+	Username string `gorm:"not null" json:"username"`
 }
