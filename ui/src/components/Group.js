ea18f2cc77a2cf83df63ce704f8a3c407ae07fbd
Nathan Osman <nathan@quickmediasolutions.com>
2022-11-12 21:26:46 -0800

    Adjust sort order for lamps.

--- ui/src/components/Group.js
+++ ui/src/components/Group.js
@@ -1,19 +1,22 @@
-import styles from './Group.module.css'
+import { sortByProp } from '../lib/util'
 import Lamp from './Lamp'
+import styles from './Group.module.css'
 
 export default function Group({ provider, group, lamps }) {
   return (
     <div className={styles.group}>
       <div className={styles.title}>{group.name}</div>
       <div className={styles.lamps}>
-        {lamps.filter(l => l.group_id == group.id).map(l => (
-          <Lamp
-            key={l.id}
-            provider={provider}
-            group={group}
-            lamp={l}
-          />
-        ))}
+        {sortByProp(lamps, 'name')
+          .filter(l => l.group_id == group.id)
+          .map(l => (
+            <Lamp
+              key={l.id}
+              provider={provider}
+              group={group}
+              lamp={l}
+            />
+          ))}
       </div>
     </div>
   )
