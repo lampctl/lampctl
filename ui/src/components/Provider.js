5e3f9d2486f1eeabf69866799928557da7309762
Nathan Osman <nathan@quickmediasolutions.com>
2022-12-27 01:56:49 -0800

    Add buttons to UI for controlling all lamps in a provider.

--- ui/src/components/Provider.js
+++ ui/src/components/Provider.js
@@ -13,9 +13,24 @@ export default function Provider({ provider }) {
       .then(r => setResponse(r))
   }, [])
 
+  function setState(state) {
+    fetch(`/api/providers/${provider.id}/apply/all`, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify({
+        state,
+        duration: 1000,
+      })
+    })
+  }
+
   return (
     <div className={styles.provider}>
-      <div className={styles.title}>{provider.name}</div>
+      <div className={styles.header}>
+        <div className={styles.title}>{provider.name}</div>
+        <div className={styles.button} onClick={() => setState(true)}>On</div>
+        <div className={styles.button} onClick={() => setState(false)}>Off</div>
+      </div>
       {response === null ?
         <p>Loading...</p> :
         sortByProp(response.groups, 'name').map(g => (
